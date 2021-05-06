c675e05c130a7cabecfb2ebe218ef2f53b042214
MrSheeplip <64995222+MrSheeplip@users.noreply.github.com>
2021-05-06 11:40:54 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,4 +11,4 @@ async def on_message(message):
     if message.content == "Ping":
         await message.channel.send("Pong")
 
-client.run("ODM5MTgwOTg3NTQzMDYwNTYw.YJF6Mg.hWRcx4XKPyhgPJkwo7nmzIfTmkc")
+client.run("")
